c28907b7252f8738822b293de016e7a5b57a55fe
liutaifa <liutaifa0920@163.com>
2019-11-21 16:38:57 +0800

    首页骨架

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,47 @@
 <template>
   <div class="home">
+    <!-- ---------------------- -->
     <!-- 模板组件 不可使用 开发删除即可 -->
-    <init-template></init-template>
+    <!-- <init-template></init-template> -->
+
+    <!-- Title -->
+    <div>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+
+    <!-- Function -->
+    <div>dsfsdfsdf</div>
+
+    <!-- Attendance -->
+    <div>
+      <div>
+        <div>
+          <p></p>
+          <i></i>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Tip -->
+    <div>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+
+    <!-- parentClass -->
+    <div>
+      <div>asd</div>
+      <div></div>
+    </div>
+
+    <!-- ---------------------- -->
   </div>
 </template>
 
@@ -9,7 +49,7 @@
 export default {
   name: "home",
   components: {
-    initTemplate: () => import("@/components/init.vue")
+    // initTemplate: () => import("@/components/init.vue")
   }
 };
 </script>
